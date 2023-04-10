31c5cf32ce99d05eff700bde4f5a7a6e49d80dd6
Vitoria Chalub <mateusfaissal@gmail.com>
2023-04-10 17:30:37 -0300

    versao inacabada com data-tests

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let numCartas = prompt("Com quantas cartas você deseja jogar? (Digite um número par de 4 a 14)");
+while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
+  numCartas = prompt("Valor inválido. Por favor, digite um número par de 4 a 14.");
+}
+
 function flipCard(carta) {
     const card1 = carta.querySelector(".card1");
     card1.classList.toggle("front");
